13fb652713b2ac66a3795539051a7b5b8bfc3368
John Mettraux <jmettraux@gmail.com>
2012-01-31 19:11:19 +0900

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+
+source :rubygems
+
+gem 'rails'
+gem 'rufus-scheduler'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Katmagic::Application.config.secret_token = 'eeb562b28364706b05aa7606328d906fe9f8620f19042f5ec15e056fb4a062aa2a2449377250d814454d7ac9ff874d7878fce3e5782547e9f554404903498b10'
